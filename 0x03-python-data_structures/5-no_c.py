9c0bfff3a29d6ba1c1b9af4e2c09d9af6c74cf67
rmm1001 <101560810+rmm1001@users.noreply.github.com>
2022-07-04 18:34:05 +0200

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,1 +1,8 @@
-qwerty
+#!/usr/bin/python3
+# 5-no_c.py
+
+def no_c(my_string):
+    """Remove all characters c and C from a string."""
+     copy = [x for x in my_string if x != 'c' and x != 'C']
+      copy = [x for x in my_string if x != 'c' and x != 'C']
+      return ("".join(copy))
