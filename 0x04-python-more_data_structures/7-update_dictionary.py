1e28080623939af1d16df35f0b409ec6d3df9539
rmm1001 <101560810+rmm1001@users.noreply.github.com>
2022-07-06 19:46:20 +0200

    Update 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,1 +1,5 @@
-qwertyui
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    """Replace or add key/value pairs in a dictionary."""
+    a_dictionary[key] = value
+    return (a_dictionary)
