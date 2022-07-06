93d334fe0253f704ed233e842df7ecc6f8dd3123
rmm1001 <101560810+rmm1001@users.noreply.github.com>
2022-07-06 19:43:03 +0200

    Update 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,10 @@
-werty
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    """Replace all occurrences of an element by another in a new list."""
+    new_list = my_list.copy()
+    for idx, item in enumerate(new_list):
+        if item == search:
+            new_list[idx] = replace
+
+    return new_list
