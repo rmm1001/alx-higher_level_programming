817da14fbf8a4eb7fe5f19471be4bf88a287ccc9
rmm1001 <101560810+rmm1001@users.noreply.github.com>
2022-07-06 20:12:48 +0200

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,30 +1,12 @@
 #!/usr/bin/python3
-
 def roman_to_int(roman_string):
-    """Converts a roman numeral to an integer."""
-    if (not isinstance(roman_string, str) or
-            roman_string is None):
-        return (0)
-
-    roman_dict = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
-            }
-    num = 0
-
+    roman = 0
+    n_rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    if type(roman_string) != str or roman_string is None:
+        return 0
     for i in range(len(roman_string)):
-        if roman_dict.get(roman_string[i], 0) == 0:
-            return (0)
-
-        if (i != (len(roman_string) - 1) and
-                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-                num += roman_dict[roman_string[i]] * -1
-            
+        if i > 0 and n_rom[roman_string[i - 1]] < n_rom[roman_string[i]]:
+            roman += n_rom[roman_string[i]] - (2 * n_rom[roman_string[i - 1]])
         else:
-            num += roman_dict[roman_string[i]]
-    return (num)
+            roman += n_rom[roman_string[i]]
+    return roman
