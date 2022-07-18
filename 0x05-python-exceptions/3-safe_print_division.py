9daa913bb395816d56af7550083955b3ffaa6738
rmm1001 <101560810+rmm1001@users.noreply.github.com>
2022-07-18 20:33:16 +0200

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,1 +1,11 @@
-qwertyui
+#!/usr/bin/python3
+def safe_print_division(a, b):
+
+    try:
+        response = a / b
+    except ZeroDivisionError:
+        response = None
+    finally:
+        print('Inside result: {}'.format(response))
+
+    return response
