5bbc9ae3306a276fffbb7ef7407db7c458309270
rmm1001 <101560810+rmm1001@users.noreply.github.com>
2022-08-04 19:09:01 +0200

    Update 6-base_geomerty.py

--- 0x0A-python-inheritance/6-base_geomerty.py
+++ 0x0A-python-inheritance/6-base_geomerty.py
@@ -1,1 +1,11 @@
-qwerty
+#!/usr/bin/python3
+"""
+Contains the class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """A class with public attribute area"""
+    def area(self):
+        """raises an exception when called"""
+        raise Exception("area() is not implemented")
